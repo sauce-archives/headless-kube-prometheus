3bc844a618d580359e5d976a09a2a501b388feff
Bertin Wong <bertin.wong@saucelabs.com>
2018-11-27 18:24:05 -0800

    added sauce dashboards

--- install-kube-prometheus.sh
+++ install-kube-prometheus.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo grafana: > dashboard-files.yaml
+echo "  serverDashboardFiles:" >> dashboard-files.yaml
+cat dashboards/*-dashboard.yaml | sed 's/^/    /' >> dashboard-files.yaml
+
 helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 helm upgrade --install --namespace monitoring --set rbacEnable=false prometheus-operator coreos/prometheus-operator
-helm upgrade --install --namespace monitoring -f headless-test.yaml kube-prometheus coreos/kube-prometheus 
+helm upgrade --install --namespace monitoring -f headless-test.yaml -f dashboard-files.yaml kube-prometheus coreos/kube-prometheus
+
+rm dashboard-files.yaml
