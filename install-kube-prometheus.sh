aef4047c6e4112d8c16b647f832c9cbc83d43ac7
Bertin Wong <bertin.wong@saucelabs.com>
2018-11-29 14:27:35 -0800

    trying to add ingress and multiple environments

--- install-kube-prometheus.sh
+++ install-kube-prometheus.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-echo grafana: > dashboard-files.yaml
-echo "  serverDashboardFiles:" >> dashboard-files.yaml
-cat dashboards/*-dashboard.yaml | sed 's/^/    /' >> dashboard-files.yaml
+k8s_env=$1 
+
+if [ "$k8s_env" == "test" ]; then
+    kubectx gke_sauce-container-beta_us-west1-c_headless-test
+    env_settings="headless-test.yaml"
+elif [  "$k8s_env" == "east1" ]; then 
+    kubectx gke_sauce-container-beta_us-east1-b_us-east1
+    env_settings="headless-east1.yaml"
+else
+    echo "Unrecognized Environment: $k8s_env"
+    exit 1
+fi
+
+echo grafana: > custom-dashboards.yaml
+echo "  serverDashboardFiles:" >> custom-dashboards.yaml
+cat dashboards/*-dashboard.yaml | sed 's/^/    /' >> custom-dashboards.yaml
 
 helm repo add coreos https://s3-eu-west-1.amazonaws.com/coreos-charts/stable/
 helm upgrade --install --namespace monitoring --set rbacEnable=false prometheus-operator coreos/prometheus-operator
-helm upgrade --install --namespace monitoring -f headless-test.yaml -f dashboard-files.yaml kube-prometheus coreos/kube-prometheus
 
-rm dashboard-files.yaml
+helm upgrade --install --namespace monitoring \
+    -f headless-settings.yaml \
+    -f "$env_settings" \
+    -f custom-dashboards.yaml \
+    kube-prometheus coreos/kube-prometheus
+
+rm custom-dashboards.yaml
